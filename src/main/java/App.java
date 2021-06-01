fc371a261e3bc68bebe561e6c46b4eb5742c919f
OderoOluoch <oderoluoch@gmail.com>
2021-06-01 14:55:57 +0300

    Reusing templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,45 +1,20 @@
-import static  spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class App {
     public static void main(String[] args) {
         staticFileLocation("/public");
-        get("/",(request,response) ->
-                        "<!DOCTYPE html>" +
-                                "<html>" +
-                                "<head>" +
-                                "<title>Hello Friend!</title>" +
-                                "<link rel='stylesheet' + href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                                "</head>" +
-                                "<body>" +
-                                "<h1>Hello From Afar</h1>" +
-                                "<p>Dear Friend,</p>" +
-                                "<p>How are you? I hope that you are having a nice weekend. I'm vacationing in the Iceland while I learn programming! </p>" +
-                                "<p>Friend, you would not believe how cold it is here. I should have gone to Hawaii instead.</p>" +
-                                "<p>But I like programming a lot, so I've got that going for me. </p>" +
-                                "<p>Looking forward to seeing you soon. I'll bring you back a souvenir. </p>" +
-                                "<p>Cheers,</p>" +
-                                "<p>Travel Enthusiast Jane</p>" +
-                                "<p><a href='/photos' >P.S. Check out my favorite travel photos here.</a></p>" +
-                                "</body>" +
-                                "</html>"
-                );
+        get("/",(request,response) -> {
+            return new ModelAndView(new HashMap(),"hello.hbs");
+        },new HandlebarsTemplateEngine());
 
-        get("photos",(request,response)->
-            "<!DOCTYPE html>" +
-                    "<html>" +
-                    "<head>" +
-                    "<title>Hello Friend!</title>" +
-                    "<link rel='stylesheet'  href='https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'>" +
-                    "</head>" +
-                    "<body>" +
-                    "<h1>Favorite Traveling Photos</h1>" +
-                    "<ul>" +
-                    "<li><img src='/images/one.jpg' alt='A photo of a mountain range.'/></li>" +
-                    "<li><img src='/images/two.jpg' alt='A photo of a swamp.'/></li>" +
-                    "<li><img src='/images/three.jpg' alt='A photo of a swamp.'/></li>" +
-                    "</ul>" +
-                    "</body>" +
-                    "</html>"
-        );
+        get("photos",(request,response)-> {
+            return new ModelAndView(new HashMap(),"photos.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
